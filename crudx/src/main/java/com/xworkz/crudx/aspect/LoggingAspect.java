5e02dc78a78c840626c38b4ff8c2879326b30067
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-11-28 12:59:23 +0530

    feat: used multiple pointcut expression for each advice type

--- crudx/src/main/java/com/xworkz/crudx/aspect/LoggingAspect.java
+++ crudx/src/main/java/com/xworkz/crudx/aspect/LoggingAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -17,13 +18,19 @@ public class LoggingAspect {
         log.info("Logging aspect constructor");
     }
 
-    @Before("execution(* com.xworkz.crudx.service.*.*(..))")
+    @Pointcut("execution(* com.xworkz.crudx.restcontroller.*.*(..))")
+    public void allRestControllerMethods(){}
+
+    @Pointcut("execution(* com.xworkz.crudx.service.*.*(..))")
+    public void allServiceMethods(){}
+
+    @Before("allRestControllerMethods() || allServiceMethods()")
     public void logBefore(JoinPoint joinPoint)
     {
          log.info("Before execution: "+joinPoint.getSignature().getName()+" method in "+joinPoint.getTarget().getClass().getSimpleName());
     }
 
-    @After("execution(* com.xworkz.crudx.service.*.*(..))")
+    @After("allRestControllerMethods() || allServiceMethods()")
     public void logAfter(JoinPoint joinPoint)
     {
         log.info("After execution: "+joinPoint.getSignature().getName()+" method in "+joinPoint.getTarget().getClass().getSimpleName());
